6b6cd40ff297fa303424aacefe2643e367670d92
Jean-Francois Theroux <failshell@gmail.com>
2013-05-24 15:53:08 -0400

    fixed version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default[:nginx][:worker_conns] = 1024
 default[:nginx][:gzip] = 'on'
 default[:nginx][:default_vhost][:enable] = true
 default[:nginx][:doc_root] = '/var/www'
-default[:nginx][:version] = '1.4.1'
+default[:nginx][:version] = '1.4.1-1.el6.ngx'
