05115a75abbe23415c201776a7a9028d3754bf95
Jean-Francois Theroux <failshell@gmail.com>
2013-05-24 14:14:26 -0400

    foodcritic compliant. added minitests.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['nginx']['workers'] = 1
-default['nginx']['worker_conns'] = 1024
-default['nginx']['gzip'] = 'on'
-default['nginx']['default_vhost']['enable'] = true
-default['nginx']['doc_root'] = '/var/www'
+default[:nginx][:workers] = 1
+default[:nginx][:worker_conns] = 1024
+default[:nginx][:gzip] = 'on'
+default[:nginx][:default_vhost][:enable] = true
+default[:nginx][:doc_root] = '/var/www'
--- providers/vhost.rb
+++ providers/vhost.rb
@@ -12,13 +12,15 @@ action :enable do
   template "/etc/nginx/vhosts.d/#{new_resource.name}.conf" do
     source "nginx/vhosts/#{new_resource.name}.conf.erb"
     mode 0644
-    notifies :restart, 'service[nginx]'
+    notifies :reload, 'service[nginx]'
   end
+  new_resource.updated_by_last_action(true)
 end
 
 action :disable do
   file "/etc/nginx/vhosts.d/#{new_resource.name}.conf" do
     action :delete
-    notifies :restart, 'service[nginx]'
+    notifies :reload, 'service[nginx]'
   end
+  new_resource.updated_by_last_action(true)
 end
