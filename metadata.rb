79866c41d30a61146473380ac3fcb6fc39392667
Jean-Francois Theroux <failshell@gmail.com>
2013-09-30 20:43:46 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'nginx'
 maintainer       'Jean-Francois Theroux'
-maintainer_email 'failshell@gmail.com'
+maintainer_email 'me@failshell.io'
 license          'Apache 2.0'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
